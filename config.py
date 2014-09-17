6af1e29744389b60ff965aa12a9e477816f6899a
Eran Cohen <eran@stratoscale.com>
2014-09-17 10:50:51 +0300

    after codereview.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-FEDORA_UPDATE_BASE_URL = 'rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/{release}/{arch}/'
-CENTOS_UPDATE_BASE_URL = 'rsync://centos.eecs.wsu.edu/centos/{release}/updates/{arch}/'
+sources = { 'fedora' : 'rsync://dl.fedoraproject.org/fedora-enchilada/linux/updates/%(release)s/%(arch)s/',
+            'centos' : 'rsync://centos.eecs.wsu.edu/centos/%(release)s/updates/%(arch)s/'
+          }
 S3_BUCKET = 's3://yumfreeze.com'
-
--- yum-snapshot.py
+++ yum-snapshot.py
@@ -2,50 +2,48 @@
 
 import argparse
 import config
+import logging
 import os
 import time
+import re
 import shutil
 import subprocess
 
 
-def addSnapshot( args ):
-    source = getSnapshotSource( args.distro, args.release, args.arch )
-    repoLocalDir = createRepoLocalDir( args.distro, args.release, args.arch )
-    print 'Creating mirror'
+def createSnapshot( distro, release, arch, cwd = None ):
+    source = getSnapshotSource( distro, release, arch )
+    repoLocalDir = createRepoLocalDir( distro, release, arch , cwd )
+    logging.info( 'Creating mirror' )
     createLocalmirror( source, repoLocalDir )
-    print 'Uploading to S3'
+    logging.info( 'Uploading to S3' )
     uploadToS3( repoLocalDir, config.S3_BUCKET )
     shutil.rmtree( repoLocalDir )
-    print 'Done.'
-    print 'Repository base url is: <bucket_url>/%s' % os.path.basename( repoLocalDir )
+    logging.info( 'Done.' )
+    logging.info( 'Repository base url is: <bucket_url>/%s' % os.path.basename( repoLocalDir ) )
 
 def getSnapshotSource( distro, release, arch ):
-    if distro == 'fedora':
-        source = config.FEDORA_UPDATE_BASE_URL
-    elif distro == 'centos':
-        source = config.CENTOS_UPDATE_BASE_URL
-    else:
-        raise KeyError( 'Failed to find source url for %s distro' % distro )
-    source = source.replace( '{release}', release )
-    source = source.replace( '{arch}', arch )
-    return source
+    source = config.sources[distro]
+    return source % dict( release = release, arch = arch )
 
-def createRepoLocalDir( distrao, release, arch ):
-    snapshotDir = time.strftime( "%Y-%m-%d-%H%M%S" ) + '-%s-%s-%s' % ( distrao, release, arch )
-    path = os.path.join( os.getcwd(), snapshotDir )
-    print path
+def createRepoLocalDir( distrao, release, arch, cwd = None ):
+    snapshotDir = '%s-%s-%s/' % ( distrao, release, arch ) + time.strftime( "%Y-%m-%d-%H%M%S" )
+    if not cwd:
+        cwd = os.getcwd()
+    path = os.path.join( cwd, snapshotDir )
     os.makedirs( path )
+    logging.info( 'Created %s as repo local directory' % path )
     return path
 
 def createLocalmirror( source, destination ):
     cmd = 'rsync  -avSHP --delete --exclude "local*" --exclude "isos" %(src)s %(dst)s' % dict( src = source, dst = destination )
-    subprocess.call( cmd, shell = True )
+    subprocess.check_call( cmd, shell = True )
 
 def uploadToS3( repoLocalDir, targetBucket ):
-    cmd = 's3cmd sync --no-delete-removed %(src)s %(dst)s' % dict( src = repoLocalDir, dst = targetBucket )
-    subprocess.call( cmd, shell = True )
+    src = os.path.dirname( repoLocalDir  )
+    cmd = 's3cmd sync --no-delete-removed %(src)s %(dst)s' % dict( src = src, dst = targetBucket )
+    subprocess.check_call( cmd, shell = True )
 
-def listAllSnapshots( dummy ):
+def listAllSnapshots():
     for entry in listBucket():
         if entry.endswith( '/' ):
             print entry.split( '/' )[3]
@@ -53,21 +51,28 @@ def listAllSnapshots( dummy ):
 def listBucket():
     cmd = 's3cmd ls ' + config.S3_BUCKET
     out = subprocess.check_output( cmd, shell = True )
-    return  out.split( '\n' )
+    return out.split( '\n' )
+
+def ReleaseFormat( release ):
+    if re.match("^\d\.\d$", release ) is None:
+        msg = "%s is not in the correct format #.#" % release
+        raise argparse.ArgumentTypeError(msg)
+    return release
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser( description = 'handle yum frozen repository', usage = 'yum-snapshot' + '<action>' )
     subparsers = parser.add_subparsers( dest = 'action', help = 'sub-command help' )
-    parser_add = subparsers.add_parser( 'add', help = 'add a yum update repository snapshot' )
-    parser_add.add_argument( '-distro', help = 'the distribution that will be snapshot', default = 'centos' )
-    parser_add.add_argument( '-release', help = 'the release that will be snapshot', default = '7' )
-    parser_add.add_argument( '-arch', help = 'the architecture that will be snapshot', default = 'x86_64' )
+    parser_add = subparsers.add_parser( 'create', help = 'create a yum update repository snapshot' )
+    parser_add.add_argument( '-distro', help = 'the distribution that will be snapshot, defaults to centos ', default = 'centos' )
+    parser_add.add_argument( '-release', type = ReleaseFormat, help = 'the release that will be snapshot, defaults to 7.0', default = '7.0' )
+    parser_add.add_argument( '-arch', help = 'the architecture that will be snapshot, defaults to x86_64 ', default = 'x86_64' )
+    parser_add.add_argument( '-cwd', help = 'the working directory, defaults to getcwd()', default = None )
 
     parser_list = subparsers.add_parser( 'list', help = 'list all available snapshots' )
     args = parser.parse_args()
 
-    choices = { 'add' : addSnapshot,
-                'list' : listAllSnapshots
-              }
-    action = args.action
-    choices[ action ]( args )
+    if args.action == 'create':
+        createSnapshot( args.distro, args.release, args.arch, args.cwd  )
+    elif args.action == 'list':
+        listAllSnapshots()
